3c19159669327c7707d0d24c14a27684dd956f6c
Jhony8727 <jhon-y-19@hotmail.com>
2025-05-14 21:59:06 -0300

    api crud v3

--- rotas/users.js
+++ rotas/users.js
@@ -18,6 +18,13 @@ const users =[
 router.get('/', (req, res) => {
     res.send(users);
 })
+router.get('/:id', (req, res) => {
+  const{ id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id)
+  res.send(foundUser)
+
+});
 
 router.post('/', (req, res) => {
     const user = req.body;
@@ -26,6 +33,31 @@ router.post('/', (req, res) => {
 
     res.send('${...user.first_name} has been added to the Database');
 })
+router.delete('/:id',(req, res) => {
+  const { id } = req.params;  
+
+  users = users.filter((user)=> user.id !== id)
+  res.send(`${id} deleted successfully from database`);
+
+});
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const {first_name, last_name, email} = req. body;
+  
+  const user = users.find((user) => user.id===id)
+  
+  if (first_name) user.first_name =first_name;
+  
+  if (last_name) user.last_name = last_name
+  
+  if (email) user.email = email
+  
+  
+  res.send(`User with the ${id} has been updated`)
+}); 
+
+
 
 
 
